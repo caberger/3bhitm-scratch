24f289c5a8bcd9287dae9ee8c41dde2a9706324c
qrzbrz <qrzbrz@yahoo.de>
2024-04-10 14:23:17 +0200

    Entity created

---
+++ backend/src/main/java/at/htl/model/Person.java
@@ -0,0 +1,15 @@
+package at.htl.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    public String firstName;
+    public String lastName;
+}
